b514353b1a668d4f56594cbbd64a16f584c5a6be
DESKTOP-IJ0SN7K\tomon <tomonaga@mx2.mesh.ne.jp>
2017-06-21 19:12:58 +0900

    2-2-8コンストラクタ コンストラクタはインスタンスを生成するときに呼ばれる処理です 特徴は ・メソッド名の部分がクラス名と同じになる ・戻り値の宣言が存在しない 使い分けは ・インスタンス生成時に値が決まって後から変わらない ->コンストラクタで指定する ・値が後から変わるもの ->メソッドやフィールドを通じて値を設定する

--- src/StudentSample.java
+++ src/StudentSample.java
@@ -1,10 +1,33 @@
 class Student {
-	String name;
 	int score;
+	String name;
 	static final int MAX_SCORE = 100;
 	
 	String getName(){
 		return name;
+	}
+	
+	//名前と点数を渡すコンストラクタ
+	Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+	
+	//名前だけを渡すコンストラクタ（点数は0にする）
+	Student(String name){
+		//以下はStudent(String name, int score)コンストラクタを呼び出している
+		this(name, 0);  
+		/* Student(String name)コンストラクタの中に直接初期化処理を書くこともできますが
+		 * 複数のコンストラクタの中に似たようなコードを毎回書くのはメンテナンス性が悪いので
+		 * コードを共有化できる場合は最も引数が多いコンストラクタに必要な処理を記述し、
+		 * 引数の少ないコンストラクタでは、this(・・・)を使って
+		 * 別のコンストラクタを呼び出す様にするとよい
+		 * 今回のStudentクラスでは、引数が最も多い
+		 * Student(String name, int score)コンストラクタに
+		 * フィールドの初期化処理を記述し、引数の少ない
+		 * Student(String name)コンストラクタではthis(・・・)を使っています
+		 * */
+		
 	}
 	
 	void setName(String name){
@@ -29,9 +52,11 @@ class Student {
 
 public class StudentSample {
 	public static void main(String... args){
-		Student murata = new Student();
-		murata.name ="村田";
-		murata.score = 40;
+		Student murata = new Student("村田");
+		//murata.score = 40;
 		murata.printScore();
+		
+		Student okada = new Student("岡田", 80);
+		okada.printScore();
 	}
 }
